98e23ff6281a6400602c9379ffa169a5ac072183
Nika Pakravan <seyedehnika.pakravan@city.ac.uk>
2022-03-12 18:28:08 +0000

    Initial Commit

---
+++ disso/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  disso
+//
+//  Created by Nika Pakravan on 12/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
