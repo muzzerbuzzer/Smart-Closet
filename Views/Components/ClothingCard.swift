d70d80c627632a1f05f7875ce53362eb4e08b30d
Nika Pakravan <seyedehnika.pakravan@city.ac.uk>
2022-03-16 16:00:47 +0000

    Update ClothingCard.swift

--- Views/Components/ClothingCard.swift
+++ Views/Components/ClothingCard.swift
@@ -14,13 +14,36 @@ struct ClothingCard: View {
         VStack {
             AsyncImage(url: URL(string: clothes.image)) { image in
                 image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .overlay(alignment: .bottom) {
+                        Text(clothes.name)
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: 136)
+                            .padding()
+                    }
+                
             } placeholder: {
                 Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .foregroundColor(.white.opacity(0.7))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .overlay(alignment: .bottom) {
+                        Text(clothes.name)
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: 136)
+                            .padding()
+                    }
             }
         }
         .frame(width: 160, height: 217, alignment: .top)
         .background(LinearGradient(gradient: Gradient(colors:[Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
         .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 10)
     }
 }
 
