1ee9558c562155126f8d696ff0ca762b538783ae
Nika Pakravan <seyedehnika.pakravan@city.ac.uk>
2022-04-19 18:56:05 +0100

    add category button

--- Views/Detail/NewOutfitView.swift
+++ Views/Detail/NewOutfitView.swift
@@ -9,12 +9,60 @@ import Foundation
 import SwiftUI
 
 struct NewOutfitView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @State private var showCategories = false
+    
     var body: some View {
         NavigationView {
             Text("beep boop")
+            
+                .toolbar(content: {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Label("Cancel", systemImage: "xmark")
+                                .labelStyle(.iconOnly)
+                        }
+                    }
+                    
+                    ToolbarItem {
+                        Button {
+                            showCategories = true
+                        } label: {
+                            Label("Add", systemImage: "plus")
+                                .labelStyle(.iconOnly)
+                        }
+                    }
+                    
+
+                
+                     
+                    /*ToolbarItem {
+                        NavigationLink(isActive: $navigateToClothingItem) {
+                            ClothesView(clothes: clothesViewModel.closet.sorted{ $0.dateAdded < $1.dateAdded} [1])
+                                .navigationBarBackButtonHidden(true)
+                        } label: {
+                            Button {
+                                saveItem()
+                                navigateToClothingItem = true
+                            } label: {
+                                Label("Save", systemImage: "checkmark")
+                                    .labelStyle(.iconOnly)
+                            }
+                        }
+                        .disabled(name.isEmpty)
+                    }*/
+                })
+            
                 .navigationTitle("New Outfit")
+                .navigationBarTitleDisplayMode(.inline)
         }
+        .sheet(isPresented: $showCategories) {
+           CategoriesView()
         .navigationViewStyle(.stack)
+        }
     }
 }
 
@@ -23,3 +71,4 @@ struct NewOutfitView_Previews: PreviewProvider {
         NewOutfitView()
     }
 }
+
