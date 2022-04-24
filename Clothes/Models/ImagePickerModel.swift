37c3c8411178949dabb5e2006107c7ae6a2302fc
Nika Pakravan <seyedehnika.pakravan@city.ac.uk>
2022-04-24 23:48:12 +0100

    Update ImagePickerModel.swift

--- Clothes/Models/ImagePickerModel.swift
+++ Clothes/Models/ImagePickerModel.swift
@@ -48,6 +48,18 @@ struct ImagePickerModel: UIViewControllerRepresentable {
         //didFinishPickingMediaWithInfo is when the image is picked, and this is what happens after
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
            
+            /*guard let urlString = UserDefaults.standard.value(forKey: "url") as? String,
+                  let url = URL(string: urlString) else {
+                        return
+            }
+            
+            let task = URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                
+            })*/
+                
             /*image is wrapped as a UIImage, and then saving as image
             which is then grabbed and then saved in NewClothesView as the image*/
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
@@ -57,12 +69,14 @@ struct ImagePickerModel: UIViewControllerRepresentable {
             //dismisses sheet when all is completed
             parent.presentationMode.wrappedValue.dismiss()
             
-            let reference = Storage.storage().reference().child("clothesImage/item.jpg")
+            let reference = Storage.storage().reference().child("clothesImages/\(Auth.auth().currentUser?.uid ?? "")/item.png")
             var data = NSData()
             data = parent.selectedImage.jpegData(compressionQuality: 0.8)! as NSData
             
             let metaData = StorageMetadata()
             metaData.contentType = "image/png"
+            
+            //let reference = Storage.storage().reference().child("clothesImage/item.jpg")
             
             reference.putData(data as Data, metadata: metaData) { (metadata, error) in
                 if error == nil {
